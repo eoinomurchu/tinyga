489200961a04259c753ca7d4315ae2447dff10d2
Eoin Murphy <eoin@murph.ie>
2012-05-31 17:25:29 +0100

    decreased dimension of fitness and selprob arrays by 1

--- src/tiny.c
+++ src/tiny.c
@@ -5,12 +5,12 @@
 #include <string.h>
 
 bool ***populations;
-int **fitness, gen, j, k, onBits, best;
-double **selprob, sum, avg, r;
+int *fitness, gen, j, k, onBits, best;
+double *selprob, sum, avg, r;
 char **params;
 
 int selecti() {
-  for (r = (double)rand() / RAND_MAX, k = 0; r >= 0.0 && k < atoi(params[2]); r -= selprob[gen % 2][k++]);
+  for (r = (double)rand() / RAND_MAX, k = 0; r >= 0.0 && k < atoi(params[2]); r -= selprob[k++]);
   return k-1;
 }
 
@@ -24,17 +24,17 @@ void xo_and_rep_then_mut() {
 }
 
 void fitnessFunction() {
-  for (avg = 0.0, sum = 0.0, best = 0, j = 0; j < atoi(params[2]); sum += fitness[gen % 2][j], avg += ((double)fitness[gen % 2][j])/atoi(params[2]), ++j) {
+  for (avg = 0.0, sum = 0.0, best = 0, j = 0; j < atoi(params[2]); sum += fitness[j], avg += ((double)fitness[j])/atoi(params[2]), ++j) {
     for (onBits = 0, k = 0; k < atoi(params[1]); onBits += populations[gen % 2][j][k++]);
-    fitness[gen % 2][j] = atoi(params[1]) - onBits;
-    best = fitness[gen % 2][j] < fitness[gen % 2][best] ? j : best;
+    fitness[j] = atoi(params[1]) - onBits;
+    best = fitness[j] < fitness[best] ? j : best;
   }
   for (j = 0; j < atoi(params[2]); j++)
-    selprob[gen % 2][j] = (atoi(params[1])-fitness[gen % 2][j])/(atoi(params[1])*atoi(params[2])-sum);
+    selprob[j] = (atoi(params[1])-fitness[j])/(atoi(params[1])*atoi(params[2])-sum);
 }
 
 void print() {
-  printf("%-15d%-15f%-15d", gen, avg, fitness[gen % 2][best]);
+  printf("%-15d%-15f%-15d", gen, avg, fitness[best]);
   for (j = 0; j < atoi(params[1]); j++)
     printf("%d", populations[gen % 2][best][j]);
   printf("\n");
@@ -47,21 +47,20 @@ void allocateNdArrays(void **array, long baseSize, int dimensions, int paramLeve
 }
 
 /* 1 Len  2 Popsize  3 Gens  4 xo  5 mut  6 seed */
-int main (int argc, char **args) {
+int main(int argc, char **args) {
   params = args;
   srand(atoi(params[6]));
   allocateNdArrays((void**)&populations, sizeof(bool), 3, 3); 
-  allocateNdArrays((void**)&fitness, sizeof(int), 2, 3); 
-  allocateNdArrays((void**)&selprob, sizeof(double), 2, 3);
+  allocateNdArrays((void*)&fitness, sizeof(int), 1, 2); 
+  allocateNdArrays((void*)&selprob, sizeof(double), 1, 2);
   for (j = 0; j < atoi(params[2])*atoi(params[1]); j++)
     populations[0][j / atoi(params[1])][j % atoi(params[1])] = rand() % 2;
   printf("Generation     Avg. Fitness   Best Fitness   Best Individual\n");
-  for (gen = 0; gen < atoi(params[3]) && (gen > 0 ? fitness[(gen-1) % 2][best] > 0 : 1); gen++) {
+  for (gen = 0; gen <= atoi(params[3]) && (gen > 0 ? fitness[best] > 0 : 1); gen++) {
     fitnessFunction();
     print();
     xo_and_rep_then_mut();
   }
-  if (gen == atoi(params[3])) { fitnessFunction(); print(); }
-  else printf("Success!\n");
+  printf(fitness[best] > 0 ? "Finished\n" : "Success!\n");
   return 0;
 }
